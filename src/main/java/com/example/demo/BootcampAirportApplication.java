d6b6dfed686116a9673286dbc768d9652e5f37dc
DELL <mcarrow002@gmail.com>
2020-07-10 15:23:01 +0300

    Project for Luggage and Boarding Pass

---
+++ src/main/java/com/example/demo/BootcampAirportApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampAirportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampAirportApplication.class, args);
+	}
+
+}
