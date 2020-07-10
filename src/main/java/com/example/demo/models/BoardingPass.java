a4d6cd0a7bf13b1f40d935a6562421557e904461
DELL <mcarrow002@gmail.com>
2020-07-10 15:25:54 +0300

    feat(Luggage BoardingPass added models)

---
+++ src/main/java/com/example/demo/models/BoardingPass.java
@@ -0,0 +1,86 @@
+package com.example.demo.models;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Table(name = "BoardingPass")
+@Entity
+public class BoardingPass {
+	
+	//BOARDING PASS ID
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="ID_BoardingPass")
+	private int ID_BPass;
+	
+	//FLIGHT
+	@Column(name="ID_Flight")
+	private Flight flight;
+	
+	
+	//USER
+	@ManyToOne
+	@JoinColumn(name="ID_User")
+	private User user;
+	
+	
+	//SEAT
+	@Column(name="Seat")
+	private int seat;
+	
+	
+	//LUGGAGE
+	@OneToMany(mappedBy="boardingPass")
+	private Collection<Luggage> allLuggage;
+
+
+	public BoardingPass() {
+		
+	}
+	public BoardingPass(Flight flight, User user, int seat, Collection<Luggage> allLuggage) {
+		super();
+		this.flight = flight;
+		this.user = user;
+		this.seat = seat;
+		this.allLuggage = allLuggage;
+	}
+	public Flight getFlight() {
+		return flight;
+	}
+	public void setFlight(Flight flight) {
+		this.flight = flight;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public int getSeat() {
+		return seat;
+	}
+	public void setSeat(int seat) {
+		this.seat = seat;
+	}
+	public Collection<Luggage> getAllLuggage() {
+		return allLuggage;
+	}
+	public void setAllLuggage(Collection<Luggage> allLuggage) {
+		this.allLuggage = allLuggage;
+	}
+	public int getID_BPass() {
+		return ID_BPass;
+	}
+	
+	
+	
+}
