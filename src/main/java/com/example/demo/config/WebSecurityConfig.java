505f079c4fa05bcae75206775406ee9cd4a3905c
Artis <a-p2@inbox.lv>
2020-07-10 15:24:39 +0300

    fix(Main) tested user login, enabled jpa repo for project

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/**").anonymous();
+		.antMatchers("/**").permitAll();
 		/*
 			.antMatchers("/admin").hasRole("ADMIN")
 			.antMatchers("/user").hasAnyRole("ADMIN","USER")
