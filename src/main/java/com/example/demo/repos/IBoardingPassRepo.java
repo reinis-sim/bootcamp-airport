8c9725eec9d166e83ffc312730bc716aaba92803
DELL <mcarrow002@gmail.com>
2020-07-13 09:56:59 +0300

    upd(IBoardingPassService BoardingPassSerivceImpl) feat(IBoardingPassRepo)

---
+++ src/main/java/com/example/demo/repos/IBoardingPassRepo.java
@@ -0,0 +1,17 @@
+package com.example.demo.repos;
+
+import java.util.ArrayList;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.models.BoardingPass;
+import com.example.demo.models.Flight;
+
+
+public interface IBoardingPassRepo extends CrudRepository<BoardingPass, Integer> {
+
+	boolean existsByFlight(Flight flight);
+
+	ArrayList<BoardingPass> findByFlight(Flight flight);
+
+}
--- src/main/java/com/example/demo/services/IBoardingPassService.java
+++ src/main/java/com/example/demo/services/IBoardingPassService.java
@@ -1,5 +1,27 @@
 package com.example.demo.services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.example.demo.models.BoardingPass;
+import com.example.demo.models.Flight;
+import com.example.demo.models.Luggage;
+import com.example.demo.models.User;
+
 public interface IBoardingPassService {
 
+	
+	//select
+	BoardingPass selectOneBoardingPassById(int id) throws Exception;
+	ArrayList<BoardingPass> selectAllBoardingPasses();
+	ArrayList<BoardingPass> selectAllBoardingPassesByFlight(Flight flight);
+	
+	//create
+	boolean insertNewBoardingPass(Flight flight, User user, int seat, Collection<Luggage> allLuggage);
+	boolean insertNewBoardingPassByObject(BoardingPass boardingPass);
+	
+	//save testing data
+	void saveTestingData();
+	
+	
 }
