5ba950f6b6545136e3da8745bc5cd049fde8d145
reinis-sim <reinissimsonsrs@gmail.com>
2020-07-14 15:57:28 +0300

    upd(flightController) fix(properties for DB)

--- src/main/java/com/example/demo/controllers/FlightController.java
+++ src/main/java/com/example/demo/controllers/FlightController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.services.IFlightService;
@@ -18,8 +19,22 @@ public class FlightController {
 	@GetMapping("/showAll") // url - localhost:8080/flight/showAll
 	public String getShowAllFlights(Model model) {
 		
-		model.addAttribute("innerObject", flightService.showAllFlights());
+		model.addAttribute("flights", flightService.showAllFlights());
 		return "show-all-flights-page";// show-all-flights-page.html
+	}
+	
+	
+	@GetMapping("/showAll/{id}")//url address->localhost:8080/flight/showAll/1
+	public String getShowAllFlightsId(@PathVariable(name = "id") int id, Model model) {
+		try
+		{
+			model.addAttribute("innerObject", flightService.selectOneFlightById(id));
+			return "show-one-flight-page";// show-one-flight-page.html
+			
+		}
+		catch (Exception e) {
+			return "error";
+		}
 	}
 	
 
